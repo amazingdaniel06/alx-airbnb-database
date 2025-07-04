010a985ea7b71fa1cef07b62dd92577cc096974e
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-04 20:56:23 +0000

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,16 @@
+ SELECT 
+    user.user_id,
+    user.first_name,
+    user.email,
+    COUNT(booking.booking_id) AS total_bookings
+FROM 
+    user
+LEFT JOIN 
+    booking
+ON 
+    user.user_id = booking.user_id
+GROUP BY 
+    user.user_id,
+    user.first_name,
+    user.email;
 

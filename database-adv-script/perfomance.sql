f67239b28dbabf22e76fb8d0f15889d49303e07a
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-04 21:47:58 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,26 @@
+SELECT
+    booking.booking_id,
+    booking.start_date,
+    booking.end_date,
+    
+    user.user_id,
+    user.first_name,
+    user.email,
+    
+    property.property_id,
+    property.name AS property_name,
+    
+    payment.payment_id,
+    payment.amount,
+    payment.payment_method,
+    payment.booking_id
+    
+FROM
+    booking
+INNER JOIN user
+    ON booking.user_id = user.user_id
+INNER JOIN property
+    ON booking.property_id = property.property_id
+LEFT JOIN payment
+    ON booking.booking_id = payment.booking_id;
 

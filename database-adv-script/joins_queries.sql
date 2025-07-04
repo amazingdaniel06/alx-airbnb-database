7a1bc2a716b2c97c97ed3da3b8cbc5741b7f06a9
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-04 20:22:49 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -24,3 +24,58 @@ LEFT JOIN
     review
 ON 
     property.property_id = review.property_id;
+
+SELECT 
+    booking.booking_id,
+    booking.property_id,
+    booking.start_date,
+    user.user_id,
+    user.first_name,
+    user.email
+FROM 
+    booking
+INNER JOIN 
+    user
+ON 
+    booking.user_id = user.user_id;
+
+SELECT 
+    property.property_id,
+    property.name,
+    review.review_id,
+    review.rating,
+    review.user_id
+FROM 
+    property
+LEFT JOIN 
+    review
+ON 
+    property.property_id = review.property_id;
+
+SELECT 
+    user.user_id,
+    user.first_name,
+    booking.booking_id,
+    booking.property_id,
+    booking.start_date
+FROM 
+    user
+LEFT JOIN 
+    booking
+ON 
+    user.user_id = booking.user_id
+
+UNION
+
+SELECT 
+    user.user_id,
+    user.first_name,
+    booking.booking_id,
+    booking.property_id,
+    booking.start_date
+FROM 
+    booking
+LEFT JOIN 
+    user
+ON 
+    booking.user_id = user.user_id;

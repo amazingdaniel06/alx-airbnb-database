eed2f3c789af70df2ecd8ad0fd0aa834272b2ec7
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-04 21:00:21 +0000

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -14,3 +14,20 @@ GROUP BY
     user.first_name,
     user.email;
 
+SELECT
+    pb.property_id,
+    property.name AS property_name,
+    pb.total_bookings,
+    ROW_NUMBER() OVER (ORDER BY pb.total_bookings DESC) AS row_number_rank,
+    RANK() OVER (ORDER BY pb.total_bookings DESC) AS rank_rank
+FROM (
+    SELECT
+        booking.property_id,
+        COUNT(booking.booking_id) AS total_bookings
+    FROM
+        booking
+    GROUP BY
+        booking.property_id
+) AS pb
+JOIN property
+  ON pb.property_id = property.property_id;

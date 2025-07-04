e3f746b41e35aa5b720de8e3a690f9810a9f3471
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-04 12:51:45 +0000

    Update joins_querie.sql

--- database-adv-script/joins_querie.sql
+++ database-adv-script/joins_querie.sql
@@ -1,1 +1,14 @@
+SELECT 
+    booking.booking_id,
+    booking.property_id,
+    booking.start_date,
+    user.user_id,
+    user.first_name,
+    user.email
+FROM 
+    booking
+INNER JOIN 
+    user
+ON 
+    booking.user_id = user.user_id;
 

f6cf5119ef72d20f433f834b688182207a7cccf1
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-04 20:47:57 +0000

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,17 @@
+SELECT 
+    property.property_id,
+    property.name
+FROM 
+    property
+WHERE 
+    property.property_id IN (
+        SELECT 
+            review.property_id
+        FROM 
+            review
+        GROUP BY 
+            review.property_id
+        HAVING 
+            AVG(review.rating) > 4.0
+    );
 

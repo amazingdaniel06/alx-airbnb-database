add957dc266f6f714c5f85bf2352c25255799cbb
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-04 12:56:47 +0000

    Update joins_querie.sql

--- database-adv-script/joins_querie.sql
+++ database-adv-script/joins_querie.sql
@@ -12,3 +12,43 @@ INNER JOIN
 ON 
     booking.user_id = user.user_id;
 
+SELECT 
+    property.property_id,
+    property.name AS property_name,
+    review.review_id,
+    review.rating,
+    review.user_id
+FROM 
+    property
+LEFT JOIN 
+    review
+ON 
+    property.property_id = review.property_id;
+
+SELECT 
+    user.user_id,
+    user.first_name,
+    booking.booking_id,
+    booking.property_id,
+    booking.start_date
+FROM 
+    user
+LEFT JOIN 
+    booking
+ON 
+    user.user_id = booking.user_id
+
+UNION
+
+SELECT 
+    user.user_id,
+    user.first_name,
+    booking.booking_id,
+    booking.property_id,
+    booking.start_date
+FROM 
+    booking
+LEFT JOIN 
+    user
+ON 
+    booking.user_id = user.user_id;
